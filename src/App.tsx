e82fcb2724e66c5f69c2a4df032c4d7dd200f523
Cairhiin <daemonaz@kymp.net>
2024-06-19 16:19:21 +0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Hero from "./Components/Hero";
+import Projects from "./Components/Projects";
+import "./App.css";
+
+function App(): JSX.Element {
+  return (
+    <>
+      <Hero />
+      <Projects />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Hero/index.tsx
@@ -0,0 +1,19 @@
+import "./index.css";
+
+const HeroSection = (): JSX.Element => {
+  return (
+    <>
+      <section id="hero">
+        <div>
+          <h2>Lorem ipsum</h2>
+          <p>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HeroSection;
