cb4e410d6473d945f6cb1d380da7e7a303deeb82
Cairhiin <daemonaz@kymp.net>
2024-06-21 11:59:08 +0300

    Finalize the layout of the projects section

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -1,6 +1,7 @@
 import DungeonAndCats from "../assets/images/dungeonAndCats.png";
 import Reimari from "../assets/images/reimari.png";
 import Bookrater from "../assets/images/bookrater.png";
+import Documenter from "../assets/images/documenter.png";
 
 export type ProjectsData = {
   id: number;
@@ -14,6 +15,15 @@ export type ProjectsData = {
 export const projectsData: ProjectsData[] = [
   {
     id: 1,
+    name: "Documenter",
+    content:
+      "Full stack work on a mediator application that allows divorce mediators to manage difficult divorce settlements cases in cooperation with clients developed by WorthitIT.",
+    img: Documenter,
+    links: "https://version.aalto.fi/gitlab/vandef2/dungeon-and-cats",
+    tech: ["Laravel", "Inertia", "Vue"],
+  },
+  {
+    id: 2,
     name: "Dungeon and Cats",
     content:
       "A topdown RPG roguelike made in C++ with SDL2. The game features a light weight entity component system.",
@@ -22,7 +32,7 @@ export const projectsData: ProjectsData[] = [
     tech: ["SDL2", "SDL Image", "SDL Mixer", "SDL TTF"],
   },
   {
-    id: 2,
+    id: 3,
     name: "Bookrater",
     content:
       "A MEAN stack project for rating books. Browse through books, rate them and see how others rate the books you enjoy.",
@@ -32,7 +42,7 @@ export const projectsData: ProjectsData[] = [
     tech: ["ExpressJS", "Mongoose", "Angular", "TypeScript", "Sass"],
   },
   {
-    id: 3,
+    id: 4,
     name: "Reimari",
     content:
       "A wordpress website for a local newspaper that is distributed weekly.",
